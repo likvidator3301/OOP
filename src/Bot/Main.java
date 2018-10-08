ecf6a9c296da75e41c858848f3d16b176f734154
likvidator4 <isakovanatolii2809@gmail.com>
2018-10-08 17:35:53 +0500

    Merge branch 'master' of C:\Data\IT Bot with conflicts.

--- src/Bot/Main.java
+++ src/Bot/Main.java
@@ -3,13 +3,16 @@ package Bot;
 import Bot.Console;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 
 public class Main
 {
-    public static void main(String[] args) throws FileNotFoundException {
-        var console = new Console(System.out, new StringInputStream());
-        console.addDataSource(new ApiVK());
-        console.startDialog();
+    public static void main(String[] args) throws IOException {
+        var vk = new ApiVK();
+        vk.Autorize();
+        //var console = new Console(System.out, System.in);
+        //console.addDataSource(new ApiVK());
+        //console.startDialog();
     }
 }
